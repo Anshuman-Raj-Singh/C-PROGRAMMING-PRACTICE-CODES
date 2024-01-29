fd5d313e4872943b2f8bed81a14426a4bf9d2b2d
ANSHUMAN RAJ SINGH <146862435+Anshuman-Raj-Singh@users.noreply.github.com>
2024-01-29 16:55:57 +0530

    function.c

---
+++ function.c
@@ -0,0 +1,11 @@
+#include<stdio.h>
+//fucntion declaration
+void printhello();
+//function call
+int main(){
+    printhello();
+    return 0;}
+//function definition
+void printhello(){
+    printf("hello\ni am anshuman\ni am going to be an army sf soldier");
+}
