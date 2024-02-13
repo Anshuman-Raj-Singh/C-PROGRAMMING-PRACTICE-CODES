d00c0af3062827271ee2afb3e7ba461c2a745394
ANSHUMAN RAJ SINGH <146862435+Anshuman-Raj-Singh@users.noreply.github.com>
2024-02-13 07:50:27 +0530

    strcpy.c

---
+++ strcpy.c
@@ -0,0 +1,11 @@
+#include<stdio.h>
+#include<string.h>
+
+int main(){
+    char str1[]="ANSHUMAN";
+    char str2[]="LIEUTINANT";
+    strcpy(str1,str2);
+    puts(str1);
+    puts(str2);
+    return 0;
+}
