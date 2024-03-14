58d38d5eb278629baee4af14ef7cd6125f4ade98
ANSHUMAN RAJ SINGH <146862435+Anshuman-Raj-Singh@users.noreply.github.com>
2024-03-14 20:27:03 +0530

    sumofodddigits.c

---
+++ sumofodddigits.c
@@ -0,0 +1,16 @@
+#include<stdio.h>
+int main(){
+    int n;
+    printf("enter n:");
+    scanf("%d",&n);
+    int sum=0;
+    while(n!=0){
+        int lastdigit=n%10;
+        n=n/10;
+        if(lastdigit%2!=0){
+            sum+=lastdigit;
+        }
+    }
+    printf("%d",sum);
+    return 0;
+}
